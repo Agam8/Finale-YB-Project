062da722413522c59fcbef6edddccd937944f39c
Agam8 <89981439+Agam8@users.noreply.github.com>
2023-01-23 22:38:09 +0200

    first version
    server started login

---
+++ server.py
@@ -0,0 +1,49 @@
+import sqlite3
+import socket
+
+IP = '0.0.0.0'
+PORT = 8888
+def handle_client_msg(request):
+    
+
+def login(cli_sock):
+
+def handle_client(cli_sock,i,addr):
+    print(f'client #{i} connected from {addr}')
+    login 
+
+def main():
+    threads = []
+    srv_sock = socket.socket()
+
+    # assigning an ip address and port number to a socket instance
+    srv_sock.bind((IP,PORT))
+
+    srv_sock.listen(20)
+    print('after listen ... start accepting')
+
+    # next line release the port
+    srv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    i = 1
+    while True:
+        print('Main thread: before accepting ...')
+        cli_sock, addr = srv_sock.accept()
+        t = threading.Thread(target=handle_client, args=(cli_sock, str(i), addr))
+        t.start()
+        i += 1
+        threads.append(t)
+        if i > 4:  # for tests change it to 4
+            print('Main thread: going down for maintenance')
+            break
+
+    all_to_die = True
+    print('Main thread: waiting to all clients to die')
+    for t in threads:
+        t.join()
+    srv_sock.close()
+    print('Bye ..')
+
+
+if __name__ == '__main__':
+    main()
